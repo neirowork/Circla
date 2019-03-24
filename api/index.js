871edead150d7a037d2161e131cb103819afe289
Tomotada Saigusa <nirot1r@g-second.net>
2019-03-24 14:51:50 +0900

    実装前準備

--- api/index.js
+++ api/index.js
@@ -4,11 +4,13 @@ const app = express()
 import bodyParser from 'body-parser'
 
 import helloWorldModule from './routes/helloWorld'
+import circleModule from './routes/circle'
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use('/', helloWorldModule)
+app.use('/circle', circleModule)
 
 export default {
   path: '/api',
