f1a895dcdf19a87fff2bb249f7a784fa08290e18
Tomotada Saigusa <nirot1r@g-second.net>
2019-03-26 05:49:39 +0900

    メール送信だけ実装

--- api/routes/circle.js
+++ api/routes/circle.js
@@ -1,4 +1,7 @@
 import express from 'express'
+import { check, validationResult } from 'express-validator/check'
+
+import * as circleModule from '../libs/circle'
 
 const router = express.Router()
 
@@ -6,4 +9,30 @@ router.get('/', (req, res) => {
   return res.json({ message: 'Hello World!' })
 })
 
+router.post('/', (req, res) => {})
+
+router.post(
+  '/pre',
+  [
+    check('displayName')
+      .isString()
+      .not()
+      .isEmpty(),
+    check('emailAddress')
+      .isEmail()
+      .not()
+      .isEmpty()
+      .normalizeEmail()
+  ],
+  (req, res) => {
+    const validationErrors = validationResult(req).array()
+    console.log(validationErrors)
+    if (validationErrors.length !== 0) {
+      return res.status(422).json({ errors: validationErrors })
+    }
+
+    circleModule.preAdd(req.body.displayName, req.body.emailAddress, 'USER')
+    return res.status(200).json({ status: true })
+  }
+)
 export default router
