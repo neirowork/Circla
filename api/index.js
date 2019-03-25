e7e130e2520d797c00b1acc21e392cca7585f0fe
Tomotada Saigusa <nirot1r@g-second.net>
2019-03-26 05:48:39 +0900

    Hello World削除

--- api/index.js
+++ api/index.js
@@ -3,13 +3,11 @@ const app = express()
 
 import bodyParser from 'body-parser'
 
-import helloWorldModule from './routes/helloWorld'
 import circleModule from './routes/circle'
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.use('/', helloWorldModule)
 app.use('/circle', circleModule)
 
 export default {
--- api/routes/helloWorld.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-const router = express.Router()
-
-router.get('/', (req, res) => {
-  return res.json({ message: 'Hello World!' })
-})
-
-export default router
