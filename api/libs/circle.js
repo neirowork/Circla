0a06c8432dc22c18871cb60a664d8c817f40f241
Tomotada Saigusa <nirot1r@g-second.net>
2019-03-26 07:21:37 +0900

    検証できるようにした

--- api/libs/circle.js
+++ api/libs/circle.js
@@ -2,6 +2,7 @@ import crypto from 'crypto'
 
 import * as verifyModule from './verify'
 import * as mailModule from './mail'
+import * as dbModule from './db'
 
 import mailTemplate from '../assets/mail'
 
@@ -9,25 +10,113 @@ import mailTemplate from '../assets/mail'
  * CirclaIdを作成する
  * @param {string} circlaId
  * @param {string} displayName
- * @param {string} emailAdderss
+ * @param {string} emailAddress
  * @param {string} scope
  */
-export const add = (circlaId, displayName, emailAdderss, scope) => {}
+export const add = (circlaId, displayName, emailAddress, scope) => {}
 
 /**
  * 仮のCirclaIdを生成する
  * @param {string} displayName
- * @param {string} emailAdderss
+ * @param {string} emailAddress
  * @param {string} scope
  */
-export const preAdd = (displayName, emailAdderss, scope) => {
+export const preAdd = (displayName, emailAddress, scope) => {
   /* サークル名とメールアドレス、スコープをDBに書き込み */
+  dbModule.getPool().getConnection((err, con) => {
+    if (err) {
+      console.error(err)
+    }
 
-  const verifyHash = verifyModule.create(createHash(1)) // Idを元に検証用コード生成
+    con.query(
+      {
+        sql: 'INSERT INTO circles SET ?',
+        values: { displayName, emailAddress, scope }
+      },
+      (err, res) => {
+        con.release()
 
-  const template = mailTemplate.verify(displayName, verifyHash)
-  mailModule.send(emailAdderss, template.subject, template.text)
+        if (err) {
+          console.error(err)
+        }
+
+        const circleHash = createHash(res.insertId)
+        const verifyHash = verifyModule.create(circleHash) // circleHashを元に検証用コード生成
+
+        const template = mailTemplate.verify(displayName, verifyHash)
+        mailModule.send(emailAddress, template.subject, template.text)
+      }
+    )
+  })
 }
+
+/**
+ * 検証用コードからCirclaIDを作成
+ * @param {string} verifyHash
+ * @param {string} circlaId
+ */
+export const verifyAdd = (verifyHash, circlaId) =>
+  new Promise(async (resolve, reject) => {
+    const circleHash = await verifyModule.getCircle(verifyHash)
+    if (!circleHash) {
+      return resolve({ status: false, errors: { message: '無効なverifyHash' } })
+    }
+
+    if (!(await isExistCirclaId(circlaId))) {
+      return resolve({
+        status: false,
+        errors: { message: '既に使用されているcirclaId' }
+      })
+    }
+
+    dbModule.getPool().getConnection((err, con) => {
+      con.query(
+        {
+          sql: 'UPDATE circles SET circlaId = ? WHERE hash = ?',
+          values: [circlaId, circleHash]
+        },
+        err => {
+          con.release()
+
+          if (err) {
+            console.error(err)
+          }
+
+          verifyModule.inactive(verifyHash)
+          return resolve({ status: true })
+        }
+      )
+    })
+  })
+
+/**
+ * 指定したCircleIdが存在するか調べる
+ * @param {string} circlaId
+ */
+const isExistCirclaId = circlaId =>
+  new Promise((resolve, reject) => {
+    dbModule.getPool().getConnection((err, con) => {
+      if (err) {
+        console.error(err)
+      }
+
+      con.query(
+        {
+          sql: 'SELECT * FROM circles WHERE ?',
+          values: { circlaId }
+        },
+        (err, res) => {
+          con.release()
+
+          if (err) {
+            console.error(err)
+          }
+
+          return resolve(res.length === 0)
+        }
+      )
+    })
+  })
 
 /**
  * @param {number} circleId
@@ -37,6 +126,26 @@ const createHash = circleId => {
     .createHash('sha256')
     .update(String(circleId))
     .digest('hex')
-  /* UPDATE *** hash = circleHash WHERE id = circleId */
+
+  dbModule.getPool().getConnection((err, con) => {
+    if (err) {
+      console.error(err)
+    }
+
+    con.query(
+      {
+        sql: 'UPDATE circles SET hash = ? WHERE id = ?',
+        values: [circleHash, circleId]
+      },
+      err => {
+        con.release()
+
+        if (err) {
+          console.error(err)
+        }
+      }
+    )
+  })
+
   return circleHash
 }
--- api/libs/verify.js
+++ api/libs/verify.js
@@ -1,6 +1,8 @@
 require('date-utils')
 import crypto from 'crypto'
 
+import * as dbModule from './db'
+
 /**
  * 検証用コードを生成する
  * @param {string} circleHash
@@ -12,7 +14,26 @@ export const create = circleHash => {
     .createHash('sha512')
     .update(`${circleHash}-${timestamp}`)
     .digest('hex')
-  /* DB書き込み */
+
+  dbModule.getPool().getConnection((err, con) => {
+    if (err) {
+      console.error(err)
+    }
+
+    con.query(
+      {
+        sql: 'INSERT INTO verifies SET ?',
+        values: { circleHash, verifyHash, timestamp }
+      },
+      err => {
+        con.release()
+
+        if (err) {
+          console.error(err)
+        }
+      }
+    )
+  })
   return verifyHash
 }
 
@@ -20,4 +41,53 @@ export const create = circleHash => {
  * 検証用コードからCircleHashを取得する
  * @param {string} verifyHash
  */
-export const getCircle = verifyHash => {}
+export const getCircle = verifyHash =>
+  new Promise((resolve, reject) => {
+    dbModule.getPool().getConnection((err, con) => {
+      if (err) {
+        console.error(err)
+      }
+
+      con.query(
+        {
+          sql:
+            'SELECT * FROM verifies WHERE ? AND inactive = 0 ORDER BY id DESC LIMIT 1',
+          values: { verifyHash }
+        },
+        (err, res) => {
+          con.release()
+
+          if (err) {
+            console.error(err)
+          }
+
+          if (res.length === 0) return resolve(false)
+          return resolve(res[0].circleHash)
+        }
+      )
+    })
+  })
+
+/**
+ * verifyHashを無効にする
+ * @param {string} verifyHash
+ */
+export const inactive = verifyHash => {
+  dbModule.getPool().getConnection((err, con) => {
+    if (err) {
+      console.error(err)
+    }
+
+    con.query(
+      {
+        sql: 'UPDATE verifies SET inactive = 1 WHERE ?',
+        values: { verifyHash }
+      },
+      err => {
+        if (err) {
+          console.error(err)
+        }
+      }
+    )
+  })
+}
