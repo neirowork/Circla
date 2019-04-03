242d9bce140f6e4a9d0a6873aa3b7062745e451e
Tomotada Saigusa <nirot1r@g-second.net>
2019-04-03 19:02:54 +0900

    Revert "verifyルートの作成"
    This reverts commit c94cd525247d13494b621ebe80356878ecfb3f82.

--- api/index.js
+++ api/index.js
@@ -4,13 +4,11 @@ const app = express()
 import bodyParser from 'body-parser'
 
 import circleModule from './routes/circle'
-import verifyModule from './routes/verify'
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use('/circle', circleModule)
-app.use('/verify', verifyModule)
 
 export default {
   path: '/api',
