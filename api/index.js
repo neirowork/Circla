eccc66484cc3b2780b183c5297f26aeb55c6f8bb
Tomotada Saigusa <nirot1r@g-second.net>
2019-04-28 02:03:40 +0900

    morgan導入

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const app = express()
 import consola from 'consola'
 
 import bodyParser from 'body-parser'
+import morgan from 'morgan'
 
 import applicationsRoute from './routes/applications'
 import eventsRoute from './routes/events'
@@ -14,6 +15,7 @@ import personalInfoRoute from './routes/personalInfo'
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+app.use(morgan({ format: 'dev', immediate: true }))
 app.use('/applications', applicationsRoute)
 app.use('/events', eventsRoute)
 app.use('/accounts', accountsRoute)
