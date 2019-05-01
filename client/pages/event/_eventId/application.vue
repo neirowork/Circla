3f634f9ad9bc07d5fb40c45a9c633d18a893c2ca
Tomotada Saigusa <nirot1r@g-second.net>
2019-05-02 01:22:53 +0900

    レスポンシブ対応前

--- client/pages/event/_eventId/application.vue
+++ client/pages/event/_eventId/application.vue
@@ -6,7 +6,7 @@
         .dialog_header_label {{ event.name }} 申込みページ
       .dialog_main
 
-        .step1(v-if='step.current === 1')
+        .step1(v-if='currentStep === 1')
           h2.dialog_main_header 申込みの前に
           p.dialog_main_content
             | 申込みの前に
@@ -26,55 +26,55 @@
             br
             | ご準備が整いましたら、下の「NEXT」ボタンを押下してください。
           h2.dialog_main_footer
-            a.btn.btn-primary(@click='step.current++') NEXT >
+            a.btn.btn-primary(@click='currentStep++') NEXT >
 
-        .step2(v-if='step.current === 2')
+        .step2(v-if='currentStep === 2')
           h2.dialog_main_header 支払いID
           p.dialog_main_content
             | 参加費の支払いIDをご入力ください。
           .alert.alert-next
             | 申込み後、事務局が確認しますので、正しいIDをご入力ください。
           form.form
-            input.form_input(placeHolder='U-190501-0123456789')
+            input.form_input(placeHolder='U-190501-0123456789', v-model='application.paymoId')
           h2.dialog_main_footer
-            a.btn.btn-back(@click='step.current--') < BACK
-            a.btn.btn-next(@click='step.current++') NEXT >
+            a.btn.btn-back(@click='currentStep--') < BACK
+            a.btn.btn-next(@click='currentStep++') NEXT >
             
-        .step3(v-if='step.current === 3')
+        .step3(v-if='currentStep === 3')
           h2.dialog_main_header サークル情報
           form.form
             span.form_control
               label.form_input_label サークル名
-              input.form_input(placeHolder='地下ぐらし！')
+              input.form_input(placeHolder='地下ぐらし！', v-model='application.circleName')
             span.form_control
               label.form_input_label さーくるめい(ひらがな)
-              input.form_input(placeHolder='あんだーぐらんどぐらし')
+              input.form_input(placeHolder='あんだーぐらんどぐらし', v-model='application.circleNameKana')
           h2.dialog_main_footer
-            a.btn.btn-back(@click='step.current--') < BACK
-            a.btn.btn-next(@click='step.current++') NEXT >
+            a.btn.btn-back(@click='currentStep--') < BACK
+            a.btn.btn-next(@click='currentStep++') NEXT >
 
-        .step4(v-if='step.current === 4')
+        .step4(v-if='currentStep === 4')
           h2.dialog_main_header 頒布情報
           form.form
             span.form_control
               label.form_input_label ジャンルコード
-              input.form_input(placeHolder='300')
+              input.form_input(placeHolder='300', v-model='application.general.genreCode')
               span.form_input_help
                 | ジャンルコードは
                 a.link(href='#') こちら
                 | をご覧ください
             span.form_control
               label.form_input_label 頒布物概要
-              textarea.form_textarea(placeHolder='あんだーぐらんどぐらし')
+              textarea.form_textarea(v-model='application.general.overview')
               span.form_input_help サークル配置を検討する際に必要な情報ですので、簡潔かつ明確にお願い致します。
             span.form_control
               label.form_input_label 頒布予定数(単位なし)
-              input.form_input(placeHolder='300')
+              input.form_input(placeHolder='50', v-model='application.general.amount')
           h2.dialog_main_footer
-            a.btn.btn-back(@click='step.current--') < BACK
-            a.btn.btn-next(@click='step.current++') NEXT >
+            a.btn.btn-back(@click='currentStep--') < BACK
+            a.btn.btn-next(@click='currentStep++') NEXT >
 
-        .step5(v-if='step.current === 5')
+        .step5(v-if='currentStep === 5')
           h2.dialog_main_header 合体申込み情報
           p.dialog_main_content
             | 合体申込みについては、こちらのガイドをご覧ください。
@@ -83,15 +83,27 @@
           form.form
             span.form_control
               label.form_input_label 相手サークル アカウントID
-              input.form_input(placeHolder='ae41e4649b934ca495991b7852b855')
+              input.form_input(placeHolder='ae41e4649b934ca495991b7852b855', v-model='application.congruence.paymoId')
             span.form_control
               label.form_input_label 相手サークル 支払いID
-              input.form_input(placeHolder='U-190501-0123456789')
-
+              input.form_input(placeHolder='U-190501-0123456789', v-model='application.congruence.accountId')
           h2.dialog_main_footer
-            a.btn.btn-back(@click='step.current--') < BACK
-            a.btn.btn-next(@click='step.current++') NEXT >
-        .step6(v-if='step.current === 6')
+            a.btn.btn-back(@click='currentStep--') < BACK
+            a.btn.btn-next(@click='currentStep++') NEXT >
+
+        .step6(v-if='currentStep === 6')
+          h2.dialog_main_header 事務局使用欄
+          p.dialog_main_content
+            | 事務局から指示された場合に入力してください。
+          form.form
+            span.form_control
+              label.form_input_label 備考
+              input.form_input(v-model='application.remarks')
+          h2.dialog_main_footer
+            a.btn.btn-back(@click='currentStep--') < BACK
+            a.btn.btn-next(@click='currentStep++') NEXT >
+
+        .step7(v-if='currentStep === 7')
           h2.dialog_main_header 入力内容確認
           p.dialog_main_content
             | 不備がないか、もう一度ご確認ください。
@@ -99,53 +111,64 @@
               tr.checkTable_row
                 td.checkTable_row_section
                 td.checkTable_row_index 支払いID
-                td.checkTable_row_value U-190501-0123456789
+                td.checkTable_row_value {{ application.paymoId }}
               tr.checkTable_row
                 td.checkTable_row_section サークル情報
                 td.checkTable_row_index サークル名
-                td.checkTable_row_value 地下ぐらし！
+                td.checkTable_row_value {{ application.circleName }}
               tr.checkTable_row
                 td.checkTable_row_section
                 td.checkTable_row_index さーくるめい
-                td.checkTable_row_value あんだーぐらんどぐらし
+                td.checkTable_row_value {{ application.circleNameKana }}
               tr.checkTable_row
                 td.checkTable_row_section 頒布情報
                 td.checkTable_row_index ジャンルコード
-                td.checkTable_row_value 300
+                td.checkTable_row_value {{ application.general.genreCode }}
               tr.checkTable_row
                 td.checkTable_row_section
                 td.checkTable_row_index 頒布物概要
-                td.checkTable_row_value 300
+                td.checkTable_row_value {{ application.general.overview }}
               tr.checkTable_row
                 td.checkTable_row_section
                 td.checkTable_row_index 頒布予定数
-                td.checkTable_row_value 300
+                td.checkTable_row_value {{ application.general.amount }}
               tr.checkTable_row
                 td.checkTable_row_section 合体申込み情報
                 td.checkTable_row_index 相手サークル アカウントID
-                td.checkTable_row_value 300
+                td.checkTable_row_value {{ application.congruence.accountId }}
               tr.checkTable_row
                 td.checkTable_row_section
-                td.checkTable_row_index 相手サークル 申込みID
-                td.checkTable_row_value U-190501-0123456789
+                td.checkTable_row_index 相手サークル 支払いID
+                td.checkTable_row_value {{ application.congruence.paymoId }}
+              tr.checkTable_row
+                td.checkTable_row_section 事務局使用欄
+                td.checkTable_row_index 備考
+                td.checkTable_row_value {{ application.remarks }}
+
           h2.dialog_main_footer
-            a.btn.btn-back(@click='step.current--') < BACK
-            a.btn.btn-next(@click='step.current++') NEXT >
-        .step7(v-if='step.current === 7')
+            a.btn.btn-back(@click='currentStep--') < BACK
+            a.btn.btn-next(@click='currentStep++') NEXT >
+            
+        .step8(v-if='currentStep === 8')
           h2.dialog_main_header 申込み最終確認ページ
           p.dialog_main_content
             | 以上で完了です。
             br
             | 「PRE-APPLICATION」を押下し、仮申込みを確定してください。
           h2.dialog_main_footer
-            a.btn.btn-back(@click='step.current--') < BACK
-            a.btn.btn-primary(@click='application()') PRE-APPLICATION >
-      .dialog_footer Step {{ step.current }} / {{ step.max }}
-    .modalWrap(v-if='modal')
+            a.btn.btn-back(@click='currentStep--') < BACK
+            a.btn.btn-primary(@click='pushApplication()') PRE-APPLICATION >
+
+      .dialog_footer Step {{ currentStep }} / 8
+    .modalWrap(v-if='this.showModal')
       .modalWrap_modal
-        .modalWrap_modal_header {{ modal.header }}
-        .modalWrap_modal_content(v-html='modal.content')
-        .modalWrap_modal_footer(v-html='modal.footer')
+        .modalWrap_modal_header 仮申込みが完了しました。
+        .modalWrap_modal_content
+          | 申込みIDは「」です。
+          br
+          | 申込みが有効になると、メールにて通知されます。
+        .modalWrap_modal_footer
+          a.btn.btn-back(@click="$router.push('/event/myfes2019')") イベントページへ戻る
 </template>
 
 <script>
@@ -154,14 +177,26 @@ export default {
   data() {
     return {
       title: this.$title('申込み - イベント名'),
-      step: {
-        current: 1,
-        max: 5
-      },
+      currentStep: 1,
       event: {
         name: 'マイフェス2019 同人即売会'
       },
-      modal: null
+      application: {
+        paymoId: '',
+        circleName: '',
+        circleNameKana: '',
+        general: {
+          genreCode: '',
+          overview: '',
+          amount: ''
+        },
+        congruence: {
+          paymoId: '',
+          accountId: ''
+        },
+        remarks: ''
+      },
+      showModal: false
     }
   },
   head() {
@@ -170,12 +205,9 @@ export default {
     }
   },
   methods: {
-    application() {
-      this.modal = {
-        header: '申込みが完了しました。',
-        content: '申込みIDは、です。',
-        footer: '閉じる'
-      }
+    pushApplication() {
+      console.log(this.application)
+      this.showModal = true
     }
   }
 }
